049f4245505dd07783551e3348dd707970d5fa9c
kellysticks <kstickell@deloitte.com>
2023-10-23 13:52:41 -0400

    Reset button, more styling

--- scripts/progressbar.js
+++ scripts/progressbar.js
@@ -1,12 +1,18 @@
 const percentage = document.getElementsByTagName('a')
 const progressBar = document.getElementsByClassName('begin-width')
 Array.from(percentage).forEach(element => {
-    element.addEventListener('click', updateProgress)
+    element.addEventListener('click', updateBars)
 });
 
-function updateProgress(event){
+function updateBars(event){
     event.preventDefault()
     const progressBar = document.getElementById(event.target.parentNode.classList.value)
     console.log(progressBar)
     progressBar.style.width = event.target.innerText
+}
+
+function resetBars(){
+    Array.from(progressBar).forEach(element =>{
+        element.style.width = '40%'
+    })
 }
